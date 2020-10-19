cffa0d4b03aa648df003df1e234204ae2316b20c
Hugo Esteves <hugo.esteves@unesp.br>
2020-10-19 03:08:28 -0300

    Octal-Binário OK
    Octal -> Binário funcionando

--- Conversor_C.c
+++ Conversor_C.c
@@ -2,40 +2,94 @@
 #include <locale.h>
 #include <string.h>
 #include <math.h>
+#include <string.h>
+
+void octalBinario(long long int num) {
+	system("cls");
+	int numNovo[200], numOrdenado[200];
+	char numCharOrdem[200];
+	long int numFinal, numSalvo=num;
+	int a, b, c;
+	for(a=0;num>0;a++) {
+		numNovo[a] = num%10;
+		num/=10;
+		b=a;
+	}
+	for(a=b, c=0;a>=0;a--, c++) {
+		numOrdenado[c]=numNovo[a];
+		numCharOrdem[c]=numNovo[a];
+	}
+	printf("%ld(08) = ",numSalvo);
+	for(a=0;a<=b;a++) {
+		if(numOrdenado[a]==1) {
+			printf("001");
+		}
+		else if(numOrdenado[a]==2) {
+			printf("010");
+		}
+		else if(numOrdenado[a]==3) {
+			printf("011");
+		}
+		else if(numOrdenado[a]==4) {
+			printf("100");
+		}
+		else if(numOrdenado[a]==5) {
+			printf("101");
+		}
+		else if(numOrdenado[a]==6) {
+			printf("110");
+		}
+		else if(numOrdenado[a]==7) {
+			printf("111");
+		}
+	}
+	printf("(02)");
+}
+
+void octalDecimal(long long int num) {
+	system("cls");
+	int numNovo[200], potencia2[200], numOrdenado[200], potencia2ordenado[200], vetMult[200];
+	long int numFinal, numSalvo=num;
+	int a, b, c;
+	for(a=0;num>0;a++) {
+		numNovo[a] = num%10;
+		num/=10;
+		b=a;
+	}
+	for(a=0;a<=b;a++) {
+		potencia2[a] = pow(8, a);
+	}
+	for(a=b, c=0;a>=0;a--, c++) {
+		numOrdenado[c]=numNovo[a];
+	}
+	for(a=b, c=0;a>=0;a--, c++) {
+		potencia2ordenado[c]=potencia2[a];
+	}
+	for(a=0;a<=b;a++) {
+		vetMult[a] = potencia2ordenado[a]*numOrdenado[a];
+	}
+	for(a=0;a<=b;a++) {
+		numFinal+=vetMult[a];
+	}
+	printf("%ld(08) = %ld(10)",numSalvo, numFinal);
+}
 
 void decimalOctal(long long int num) {
-	system("cls");
 	int a, ehDivisivel=0, deuZero=0, b;
 	long long int salvaNum=num, aux, novoNum, numDiv;
 	int numNovo[50];
 	for(a=0;a<1000 || deuZero==1;a++) {
-		numDiv = num%8;
-		numNovo[a] = numDiv;
-		printf("%d ", num);
-		if(num==1) {
-			numNovo[a]==1;
-			deuZero==1;
-			aux = a;
-			break;
-		}
-		else if(num==0) {
-			numNovo[a]==0;
-			deuZero==1;
-			aux=a;
-			break;
-		}
-		num%=8;
+		
 	}
 	printf("\n");
 	//printf("%lld(10) = ", salvaNum);
 	for(a=aux;a>=0;a--) {
-		printf("%d ", numNovo[a]);
+		printf("%d", numNovo[a]);
 	}
 	//printf("(8)");
 }
 
 void decimalBinario(long long int num) {
-	system("cls");
 	int a, ehDivisivel=0, deuZero=0, b;
 	long long int salvaNum=num, aux;
 	int numNovo[50];
@@ -88,7 +142,7 @@ void binarioDecimal(long long int num) {
 	printf("%ld(10) = %ld(2)",numSalvo, numFinal);
 }
 
-int confereZeros(long long num) {
+int confereZeros(long long int num) {
 	int numNovo[200];
 	int a, b, res;
 	for(a=0;num>0;a++) {
@@ -101,6 +155,25 @@ int confereZeros(long long num) {
 			res=0;
 		}
 		else if (numNovo[a]==0 || numNovo[a]==1){
+			res=1;
+		}
+	}
+	return res;
+}
+
+int confereOito(long long int num) {
+	int numNovo[200];
+	int a, b, res;
+	for(a=0;num>0;a++) {
+		numNovo[a] = num%10;
+		num/=10;
+		b=a;
+	}
+	for(a=b;a>=0;a--) {
+		if(numNovo[a] <= -1|| numNovo[a] >= 8) {
+			res=0;
+		}
+		else if (numNovo[a]>=0 || numNovo[a]<=7){
 			res=1;
 		}
 	}
@@ -125,22 +198,32 @@ void menu() {
 		fflush(stdout);
 	} while(op<0 || op>4);
 	if (op==1) {
-		decimalOctal(num);
+		int res8 = confereOito(num);
+		if(res8==0) {
+			system("cls");
+			printf("\nVocê inseriu um número octal inválido, voltando ao menu principal...\n");
+			system("pause");
+			menu();
+		}
+		else if(res8==1){
+			octalBinario(num);
+		}
 	}
 	else if (op==2) {
-		int res = confereZeros(num);
-		if(res==0) {
+		int res0 = confereZeros(num);
+		if(res0==0) {
 			system("cls");
 			printf("\nVocê inseriu um número binário inválido, voltando ao menu principal...\n");
 			system("pause");
 			menu();
 		}
-		else if(res==1){
+		else if(res0==1){
 			binarioDecimal(num);
 		}
 	}
 	else if (op==3) {
 		decimalBinario(num);
+		decimalOctal(num);
 	}
 	else {
 		
